1996328607908e799e39724de38fc10723f5acd1
Everth Paredes <epe0413@gmail.com>
2023-05-17 14:06:04 -0600

    Validar el JWT - Servicio

--- controllers/auth.js
+++ controllers/auth.js
@@ -98,7 +98,18 @@ const googleSignIn = async ( req, res = response ) => {
 
 }
 
+const renovarToken = async( req, res = response ) => {
+
+    const { usuario } = req;
+
+    // Generar el JWT
+    const token = await generarJWT( usuario.id );
+
+    res.json({ usuario, token })
+}
+
 module.exports= {
     login,
-    googleSignIn
+    googleSignIn,
+    renovarToken
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login, googleSignIn } = require('../controllers/auth');
-const { validarCampos } = require('../middlewares/validar-campos');
+const { login, googleSignIn, renovarToken } = require('../controllers/auth');
+const { validarCampos, validarJWT } = require('../middlewares');
 
 const router = Router();
 
@@ -16,6 +16,8 @@ router.post('/google',[
     check('id_token', 'id_token es necesario').not().isEmpty(),
     validarCampos
 ], googleSignIn );
+
+router.get('/', validarJWT, renovarToken )
 
 
 module.exports = router;
